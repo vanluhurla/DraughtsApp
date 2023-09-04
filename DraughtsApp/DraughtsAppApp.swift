f9c35738640a21fbe4779933d047ee8a6035a5e0
vanluhurla <94018863+vanluhurla@users.noreply.github.com>
2023-09-04 21:03:19 +0100

    Move project to repository.

---
+++ DraughtsApp/DraughtsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraughtsAppApp.swift
+//  DraughtsApp
+//
+//  Created by Vanessa Hurla on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct DraughtsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
